a902cf308c34589314572ee180146ea3c93b61ad
CRISS NGUYEN <152765218+CrissNguyenKhanh@users.noreply.github.com>
2026-06-17 23:32:37 +0700

    feat: implement multiply operation

--- node1/khanh.java
+++ node1/khanh.java
@@ -5,8 +5,16 @@
 
                 
             }
+        private Double calculate(SIGN sign, Double arg1, Double arg2) {
+                switch (sign) {
+                    case MULTIPLY:
+                        return arg1 * arg2;
 
-
+                    default:
+                        break;
+                }
+            return 0d;
+            }
 
             
 
