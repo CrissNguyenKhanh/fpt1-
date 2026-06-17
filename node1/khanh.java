2b36e8db5a876b01701380b7910a8d76020620ad
CRISS NGUYEN <152765218+CrissNguyenKhanh@users.noreply.github.com>
2026-06-17 23:03:12 +0700

    feat: implement minus operation

--- node1/khanh.java
+++ node1/khanh.java
@@ -5,18 +5,26 @@
 
                 
             }
-
+       enum SIGN {
+            PLUS,
+            MINUS,
+            MULTIPLY,
+            DIVIDE
+}
+         
 
         private Double calculate(SIGN sign, Double arg1, Double arg2) {
             switch (sign) {
                 case PLUS:
                     return arg1 + arg2;
 
+                case MINUS:
+                    return arg1 - arg2;
+
                 default:
                     break;
             }
             return 0d;
         }
-                    
 
         }
