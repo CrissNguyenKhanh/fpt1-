054580de82c6aeb33a1756eb1fc71ed8af005dd4
CRISS NGUYEN <152765218+CrissNguyenKhanh@users.noreply.github.com>
2026-06-17 23:33:40 +0700

    feat: implement divide operation

--- node1/khanh.java
+++ node1/khanh.java
@@ -7,7 +7,19 @@
             }
 
 
+        private Double calculate(SIGN sign, Double arg1, Double arg2) {
+            switch (sign) {
+                case DIVIDE:
+                    if (arg2 == 0) {
+                        return 0d;
+                    }
+                    return arg1 / arg2;
 
-            
+                default:
+                    break;
+            }
+            return 0d;
+        }
+                    
 
         }
