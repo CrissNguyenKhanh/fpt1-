8d401e73ebd86c022b73c259f5ebc4c065c6ba81
CRISS NGUYEN <152765218+CrissNguyenKhanh@users.noreply.github.com>
2026-06-17 22:35:24 +0700

    feat: add initial implementation

--- node1/khanh.java
+++ node1/khanh.java
@@ -2,6 +2,8 @@
        public class khanh {
             public static void main(String[] args) {
                 System.out.println("Hello, World!");
-
                 
             }
+
+            
+        }
